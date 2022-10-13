ae2595a16ce89535c50e13105874f705e5629115
Github <igst@HP.com>
2022-10-13 08:44:21 +0800

    menambahkan tampilan kelas

---
+++ app/views/kelas/index.php
@@ -0,0 +1,139 @@
+  <!-- Begin Page Content -->
+  <div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-2 text-gray-800">Data Kelas</h1>
+<p class="mb-4">Ini data kelas SKENSA</p>
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+   <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">Data Kelas</h6>
+   </div>
+   <div class="card-body">
+         <div class="table-responsive p-1">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+               <thead>
+                     <tr>
+                        <th>No.</th>
+                        <th>Nomer Ruangan</th>
+                        <th>Nama Kelas</th>
+                        <th>Wali Kelas</th>
+                     </tr>
+               </thead>
+               <!-- <tfoot>
+                     <tr>
+                        <th>No.</th>
+                        <th>Nomer Ruangan</th>
+                        <th>Nama Kelas</th>
+                        <th>Wali Kelas</th>
+                     </tr>
+               </tfoot> -->
+               <tbody>
+                     <tr>
+                        <td>1</td>
+                        <td>101</td>
+                        <td>RPL</td>
+                        <td>Guy Hawkins S.Kom.</td>
+                     </tr>
+                     <tr>
+                        <td>2</td>
+                        <td>102</td>
+                        <td>MM</td>
+                        <td>Jane Cooper S.Pd.</td>
+                     </tr>
+                     <tr>
+                        <td>3</td>
+                        <td>103</td>
+                        <td>TKRO</td>
+                        <td>Wade Warren S.Kom.</td>
+                     </tr>
+                     <tr>
+                        <td>4</td>
+                        <td>104</td>
+                        <td>TBSM</td>
+                        <td>Esther Howard S.Kom.</td>
+                     </tr>
+                     <tr>
+                        <td>5</td>
+                        <td>105</td>
+                        <td>TP</td>
+                        <td>Cameron Wiliamson S.Pd.</td>
+                     </tr>
+                     <tr>
+                        <td>6</td>
+                        <td>106</td>
+                        <td>TKJ</td>
+                        <td>Brooklyn Simmons S.Pd.</td>
+                     </tr>
+                     <tr>    
+                        <td>7</td>
+                        <td>107</td>
+                        <td>TITL</td>
+                        <td>Leslie Alexander S.Kom.</td>
+                     </tr>
+                     <tr>    
+                        <td>8</td>
+                        <td>108</td>
+                        <td>TPTU</td>
+                        <td>Jenny Wilson S.Pd.</td>
+                     </tr>
+                     <tr>    
+                        <td>9</td>
+                        <td>109</td>
+                        <td>DPIB</td>
+                        <td>Guy Hawkins S.Kom.</td>
+                     </tr>
+                     <tr>    
+                        <td>10</td>
+                        <td>110</td>
+                        <td>BKP</td>
+                        <td>Wilson Maringa S.Kom.</td>
+                     </tr>
+               </tbody>
+            </table>
+         </div>
+   </div>
+</div>
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- End of Main Content -->
+
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+<div class="container my-auto">
+<div class="copyright text-center my-auto">
+<span>Copyright &copy; smkn1denpasar2022</span>
+</div>
+</div>
+</footer>
+<!-- End of Footer -->
+</div>
+<!-- End of Content Wrapper -->
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+<i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+<span aria-hidden="true">×</span>
+</button>
+</div>
+<div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+<div class="modal-footer">
+<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+<a class="btn btn-primary" href="login.html">Logout</a>
+</div>
+</div>
+</div>
+</div>
